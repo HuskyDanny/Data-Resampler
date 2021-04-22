6c09f2c9999ba2cfe475a3261d91cd3d93e1fbee
Allen Pan <allenpan@microsoft.com>
2021-04-22 14:01:51 +0800

    init project

---
+++ DataResampler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Data.HashFunction.SpookyHash;
+
+namespace HashFunction
+{
+    internal class DataResampler
+    {
+        private ISpookyHashV2 _hasher;
+
+        private int _hashBucketCount;
+        private int _hashSeed;
+
+        private int _resamplingPercentage { get; set; }
+
+        internal DataResampler(ISpookyHashV2 hasher, int hashBucketCount, int hashSeed, int resamplingPercentage)
+        {
+            this._hasher = hasher;
+            this._hashBucketCount = hashBucketCount;
+            this._hashSeed = hashSeed;
+            this._resamplingPercentage = resamplingPercentage;
+        }
+
+        public bool ShouldKeep(string tenantId, string assignmentId)
+        {
+            string hashKey = tenantId + this._hashSeed + assignmentId;
+            byte[] hashKeyBytes = Encoding.UTF8.GetBytes(hashKey);
+
+            byte[] hashBytes = this._hasher.ComputeHash(hashKeyBytes).Hash;
+
+            UInt32 hashBytesShouldRotate = 0;
+            for (int i = 0; i < sizeof(int); i++)
+            {
+                hashBytesShouldRotate <<= 8;
+                hashBytesShouldRotate |= hashBytes[i];
+            }
+
+            var rangeSize = _resamplingPercentage * 10;
+            var hashValue = (int?)(hashBytesShouldRotate % _hashBucketCount);
+            return hashValue <= (rangeSize - 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Data.HashFunction.SpookyHash;
+
+namespace HashFunction
+{
+    internal class Program
+    {
+        private const int HASHCOUNT = 1000;
+        private const int PERCENTAGE = 10;
+
+        private const int HASHSEED = 121;
+        private const int SAMPLE_DATA_SIZE = 1000000;
+
+        private static void Main(string[] args)
+        {
+            List<string> tenantIds = new List<string> { "walmart", "pepsi", "costco" };
+            List<int> tenantIdsRatio = new List<int> { 89, 10, 1 };
+
+            Dictionary<string, int> tenantIdSelectedCounts = new Dictionary<string, int>();
+            foreach (var row in PrepareSampleData(tenantIds, tenantIdsRatio))
+            {
+                string tenantId = row.Item1;
+                string assignmentId = row.Item2;
+
+                var hasher = SpookyHashV2Factory.Instance.Create(new SpookyHashConfig());
+                var dataResampler = new DataResampler(hasher, HASHCOUNT, HASHSEED, PERCENTAGE);
+
+                if (dataResampler.ShouldKeep(tenantId, assignmentId))
+                {
+                    if (!tenantIdSelectedCounts.ContainsKey(tenantId))
+                    {
+                        tenantIdSelectedCounts[tenantId] = 0;
+                    }
+                    else
+                    {
+                        tenantIdSelectedCounts[tenantId] += 1;
+                    }
+                }
+            }
+
+            int totalCount = 0;
+            foreach (var key in tenantIdSelectedCounts.Keys)
+            {
+                var count = tenantIdSelectedCounts[key];
+                Console.WriteLine("{0}TrueCount : ${1}; ", key, count);
+                totalCount += count;
+            }
+
+            Console.WriteLine("totalTrueCount : {0}", totalCount);
+        }
+
+        private static List<Tuple<string, string>> PrepareSampleData(List<string> tenantIds, List<int> tenantIdsRatio)
+        {
+            List<Tuple<string, string>> sampleData = new List<Tuple<string, string>>();
+
+            int i = 0;
+
+            while (i < SAMPLE_DATA_SIZE)
+            {
+                for (int j = 0; j < tenantIds.Count; j++)
+                {
+                    for (int k = 0; k < tenantIdsRatio[j]; k++)
+                    {
+                        sampleData.Add(Tuple.Create(tenantIds[j], Guid.NewGuid().ToString()));
+                        i += 1;
+                    }
+                }
+            }
+
+            return sampleData;
+        }
+    }
+}
